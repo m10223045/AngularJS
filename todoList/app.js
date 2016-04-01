c57e1f24afd90cd1af8a117b3d5cb9c47b30bdfa
Ian Sie <dreamsky1985@gmail.com>
2016-04-01 21:20:59 +0800

    Add AngularJS pratice files include app.js, example.html, index.html

---
+++ todoList/app.js
@@ -0,0 +1,40 @@
+( function(){
+	var app = angular.module('todoList', []);
+
+	app.controller('ListController', function(){
+		this.list = scopelist;
+		this.newItem ={};
+
+		this.addItem = function(){
+			if (this.newItem) {
+				this.newItem.isFinished=false;
+				this.list.push(this.newItem);
+				this.newItem = {};
+			};
+
+		};
+
+		this.delItem = function(item){
+			
+			item.isFinished = true;
+		};
+
+
+		this.edit = function(item){
+			item.editing = true;
+		};
+
+		this.save = function(item){
+			delete item.editing;
+		};
+
+	});
+
+	app.directive('textexample', function(){
+		return {
+			restrict: 'E',
+			templateUrl: "example.html"
+		};
+	});
+	scopelist = [{ label: "Eat Dinner", isFinished: false } , { label: "Go To Run", isFinished: false }];
+})();
